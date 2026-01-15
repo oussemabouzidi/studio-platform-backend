aa85241aec9c23de7df2a0a190b83b9f49077b55
Oussema Bouzidi <bouzidioussema16@gmail.com>
2026-01-15 17:26:52 +0100

    .

--- database.js
+++ database.js
@@ -1,15 +1,16 @@
-import mysql2 from 'mysql2'
 import dotenv from 'dotenv'
 dotenv.config()
 
 
-
-const pool = mysql2.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password : process.env.PASSWORD,
-    database: process.env.DATABASE
-}).promise()
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
 
 export default pool
